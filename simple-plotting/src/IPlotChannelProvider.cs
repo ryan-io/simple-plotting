27566d41c35e8566f146f1721097b390b6e8d394
Ryan Developer - Unity <47833404+ryanunitydeveloper@users.noreply.github.com>
2023-09-29 13:15:46 -0500

    Defined ForceSetSource in IPlotChannelProvider

--- simple-plotting/src/IPlotChannelProvider.cs
+++ simple-plotting/src/IPlotChannelProvider.cs
@@ -2,6 +2,7 @@ namespace SimplePlot;
 
 public interface IPlotChannelProvider {
 	string? Path { get; }
+	void    ForceSetSource(string? path);
 	Task<IReadOnlyList<PlotChannel>?>  ExtractAsync();
 	bool                             SetSource(string path);
 }
