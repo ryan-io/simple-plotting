608e5e3a9e48de239bfb3da3772a727c16b6983d
Ryan <ryan@ryandeveloper.io>
2023-10-06 12:04:10 -0500

    PlotEvent.cs is now structured around PlotEventData.

--- simple-plotting/src/PlotEvent.cs
+++ simple-plotting/src/PlotEvent.cs
@@ -7,14 +7,39 @@ namespace simple_plotting.src {
 		///  Event for when a record is enumerated. This will only be called if the record is valid during parsing.
 		///  It is up to the consumer to keep track of RecordEnumerated subscriptions.
 		/// </summary>
-		public static event Action<PlotChannelRecord?> RecordEnumerated;
+		public static event Action<PlotEventData?>? RecordEnumerated;
 		
 		/// <summary>
 		///  Invoke the RecordEnumerated event.
 		/// </summary>
-		/// <param name="record">Record to broadcast</param>
-		internal static void OnRecordEnumerated(PlotChannelRecord record) {
-			RecordEnumerated.Invoke(record);
+		/// <param name="data">Record to broadcast</param>
+		internal static void OnRecordEnumerated(PlotEventData data) {
+			RecordEnumerated?.Invoke(data);
+		}
+	}
+
+	/// <summary>
+	///  Data passed from event handler to subscribers
+	/// </summary>
+	public readonly struct PlotEventData {
+		/// <summary>
+		///  Channel name the record belongs to
+		/// </summary>
+		public string Channel { get; }
+		
+		/// <summary>
+		///  Event record
+		/// </summary>
+		public PlotChannelRecord Record { get; }
+
+		/// <summary>
+		///  Constructor 
+		/// </summary>
+		/// <param name="channel">Channel name the record belongs to</param>
+		/// <param name="record">Event record</param>
+		public PlotEventData(string channel, PlotChannelRecord record) {
+			Channel     = channel;
+			Record = record;
 		}
 	}
 }
