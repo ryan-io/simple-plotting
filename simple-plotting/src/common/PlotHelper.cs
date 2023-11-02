20214ce961c69dcf6361b3f72231574ae6969cc3
Ryan Developer - Unity <47833404+ryanunitydeveloper@users.noreply.github.com>
2023-11-02 10:51:59 -0500

    Added static method to map a plot size enum in PlotHelper.cs

--- simple-plotting/src/common/PlotHelper.cs
+++ simple-plotting/src/common/PlotHelper.cs
@@ -1,7 +1,15 @@
 ﻿using ScottPlot;
 
 namespace simple_plotting.src {
+	/// <summary>
+	///  Static class containing helper methods for plots.
+	/// </summary>
 	public static class PlotHelper {
+		/// <summary>
+		///  Sets the size of all plots within the enumerable via mapping of size
+		/// </summary>
+		/// <param name="plots">Enumerable of plots</param>
+		/// <param name="size">Size to set each plot to</param>
 		public static void SetSizeOfAll(IEnumerable<Plot> plots, PlotSize size) {
 			var sizeContainer = PlotSizeMapper.Map(size);
 
@@ -10,5 +18,12 @@ namespace simple_plotting.src {
 				plot.Height = sizeContainer.Height;
 			}
 		}
+		
+		/// <summary>
+		/// Takes a PlotSize and maps it to a PlotSizeContainer.
+		/// </summary>
+		/// <param name="size">PlotSize to map</param>
+		/// <returns>PlotSizeContainer containing width & height</returns>
+		public static PlotSizeContainer GetPlotSizeContainer(PlotSize size) => PlotSizeMapper.Map(size);
 	}
 }
